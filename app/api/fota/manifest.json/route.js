ed116237359757fd7d9ec8c8eacf9f24d9b08d13
Jazoolee <jazooleeahamed@gmail.com>
2025-10-10 17:16:29 +0530

    updated manifest route

--- app/api/fota/manifest.json/route.js
+++ app/api/fota/manifest.json/route.js
@@ -5,27 +5,18 @@ import path from "path";
 export async function GET() {
   try {
     // Path to the JSON file inside /manifest.json
-    const filePath = path.join(process.cwd(), "/app/api/fota/manifest.json/manifest.json");
+    const filePath = path.join(process.cwd(), "public", "manifest.json");
 
     // Read the JSON file contents
     const fileContents = await fs.readFile(filePath, "utf-8");
+    const jsonData = JSON.parse(fileContents);
 
     // Return it as a JSON response
-    return new NextResponse(fileContents, {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json"
-      },
-    });
-
+    return NextResponse.json(jsonData, { status: 200 });
   } catch (error) {
     console.error("Error reading manifest.json:", error);
     return NextResponse.json(
-      {
-        success: false,
-        error: "Failed to read manifest.json",
-        message: error.message,
-      },
+      { success: false, error: error.message },
       { status: 500 }
     );
   }
